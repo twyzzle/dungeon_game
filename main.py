4ceff32ffcd5240377fe5b8493a22b90f293bde1
twyzzle <149459564+twyzzle@users.noreply.github.com>
2023-10-31 21:37:29 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+# main.py
+
+from player import Player
+from scenery import Scenery
+from game import Game
+
+# Create instances of your classes
+player = Player()
+scenery = Scenery()
+
+# Start the game
+game = Game(player, scenery)
+game.run()
 
